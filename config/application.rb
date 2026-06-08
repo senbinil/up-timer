2119dc1966f45dd2c9cb2bf1f6ce2d4d5f60f2eb
binilsn <binil.sn@pitsolutions.com>
2026-06-08 15:47:55 +0530

    feat(config): set timezone to Asia/Kolkata

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module UpTimer
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Kolkata"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
